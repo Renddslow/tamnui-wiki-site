7492e28fde561c16bb7a8962cd89dc00b2d26c5f
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2023-11-23 22:16:55 -0600

    feat: expanded the deity box

--- scripts/infoBoxes/deity.js
+++ scripts/infoBoxes/deity.js
@@ -13,6 +13,69 @@ export const deityInfoBoxTable = (data) => {
         element.tr({}, [
           element.td({ colspan: 2 }, [element.img({ src: data?.image || "" })]),
         ]),
+        element.tr({}, [infoBoxHeader("Basic Information", "#d7d6dc")]),
+        element.tr({}, [
+          element.td({}, [text("Title(s)")]),
+          element.td({}, [
+            element.ul({}, [
+              (data?.titles ?? []).map((title) =>
+                element.li({}, [text(title)]),
+              ),
+            ]),
+          ]),
+        ]),
+        element.tr({}, [
+          element.td({}, [text("Pantheon")]),
+          element.td({}, [
+            element.ul({}, [
+              (data?.pantheons ?? []).map((pantheon) =>
+                element.li({}, [
+                  element.a(
+                    {
+                      href: `/${pantheon
+                        .toLowerCase()
+                        .replace(/\w/g, "-")}-pantheon`,
+                    },
+                    [text(`${pantheon} Pantheon`)],
+                  ),
+                ]),
+              ),
+            ]),
+          ]),
+        ]),
+        element.tr({}, [
+          element.td({}, [text("Gender")]),
+          element.td({}, [text(data?.gender ?? "")]),
+        ]),
+        element.tr({}, [infoBoxHeader("Statistics", "#d7d6dc")]),
+        element.tr({}, [
+          element.td({}, [text("Alignment")]),
+          element.td({}, [text(data?.alignment ?? "")]),
+        ]),
+        element.tr({}, [
+          element.td({}, [text("Symbol")]),
+          element.td({}, [text(data?.symbol ?? "")]),
+        ]),
+        element.tr({}, [
+          element.td({}, [text("Abode(s)")]),
+          element.td({}, [
+            element.ul({}, [
+              (data?.abodes ?? []).map((abode) =>
+                element.li({}, [text(abode)]),
+              ),
+            ]),
+          ]),
+        ]),
+        element.tr({}, [
+          element.td({}, [text("Domains")]),
+          element.td({}, [
+            element.ul({}, [
+              (data?.domains ?? []).map((domain) =>
+                element.li({}, [text(domain)]),
+              ),
+            ]),
+          ]),
+        ]),
       ]),
     ],
   );
