f81ee761d6aa087d0b41e16ea49853ebabbae833
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2023-11-23 22:29:39 -0600

    fix: fixed issues with deity infobox

--- scripts/infoBoxes/deity.js
+++ scripts/infoBoxes/deity.js
@@ -2,6 +2,7 @@ import { element, text } from "./element.js";
 import { infoBoxHeader } from "./components.js";
 
 export const deityInfoBoxTable = (data) => {
+  console.log(data);
   return element.table(
     {
       className: ["infobox race"],
@@ -17,64 +18,66 @@ export const deityInfoBoxTable = (data) => {
         element.tr({}, [
           element.td({}, [text("Title(s)")]),
           element.td({}, [
-            element.ul({}, [
+            element.ul(
+              {},
               (data?.titles ?? []).map((title) =>
                 element.li({}, [text(title)]),
               ),
-            ]),
+            ),
           ]),
         ]),
-        element.tr({}, [
-          element.td({}, [text("Pantheon")]),
-          element.td({}, [
-            element.ul({}, [
-              (data?.pantheons ?? []).map((pantheon) =>
-                element.li({}, [
-                  element.a(
-                    {
-                      href: `/${pantheon
-                        .toLowerCase()
-                        .replace(/\w/g, "-")}-pantheon`,
-                    },
-                    [text(`${pantheon} Pantheon`)],
-                  ),
-                ]),
-              ),
-            ]),
-          ]),
+      ]),
+      element.tr({}, [
+        element.td({}, [text("Pantheon")]),
+        element.td({}, [
+          element.ul(
+            {},
+            (data?.pantheons ?? []).map((pantheon) =>
+              element.li({}, [
+                element.a(
+                  {
+                    href: `/${pantheon
+                      .toLowerCase()
+                      .replace(/\w/g, "-")}-pantheon`,
+                  },
+                  [text(`${pantheon} Pantheon`)],
+                ),
+              ]),
+            ),
+          ),
         ]),
-        element.tr({}, [
-          element.td({}, [text("Gender")]),
-          element.td({}, [text(data?.gender ?? "")]),
+      ]),
+      element.tr({}, [
+        element.td({}, [text("Gender")]),
+        element.td({}, [text(data?.gender ?? "")]),
+      ]),
+      element.tr({}, [infoBoxHeader("Statistics", "#d7d6dc")]),
+      element.tr({}, [
+        element.td({}, [text("Alignment")]),
+        element.td({}, [text(data?.alignment ?? "")]),
+      ]),
+      element.tr({}, [
+        element.td({}, [text("Symbol")]),
+        element.td({}, [text(data?.symbol ?? "")]),
+      ]),
+      element.tr({}, [
+        element.td({}, [text("Abode(s)")]),
+        element.td({}, [
+          element.ul(
+            {},
+            (data?.abodes ?? []).map((abode) => element.li({}, [text(abode)])),
+          ),
         ]),
-        element.tr({}, [infoBoxHeader("Statistics", "#d7d6dc")]),
-        element.tr({}, [
-          element.td({}, [text("Alignment")]),
-          element.td({}, [text(data?.alignment ?? "")]),
-        ]),
-        element.tr({}, [
-          element.td({}, [text("Symbol")]),
-          element.td({}, [text(data?.symbol ?? "")]),
-        ]),
-        element.tr({}, [
-          element.td({}, [text("Abode(s)")]),
-          element.td({}, [
-            element.ul({}, [
-              (data?.abodes ?? []).map((abode) =>
-                element.li({}, [text(abode)]),
-              ),
-            ]),
-          ]),
-        ]),
-        element.tr({}, [
-          element.td({}, [text("Domains")]),
-          element.td({}, [
-            element.ul({}, [
-              (data?.domains ?? []).map((domain) =>
-                element.li({}, [text(domain)]),
-              ),
-            ]),
-          ]),
+      ]),
+      element.tr({}, [
+        element.td({}, [text("Domains")]),
+        element.td({}, [
+          element.ul(
+            {},
+            (data?.domains ?? []).map((domain) =>
+              element.li({}, [text(domain)]),
+            ),
+          ),
         ]),
       ]),
     ],
